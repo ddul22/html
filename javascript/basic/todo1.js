8dd0deef1496214babf7655e509ed8bc7d96f268
ddul22 <kjj0757@naver.com>
2025-01-21 09:01:37 +0900

    123

--- javascript/basic/todo1.js
+++ javascript/basic/todo1.js
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
+
 //해당 월수 임의로 기입 맞는 계절을 출력
 // ex)3월부터 6월까지 봄 6월부터 9월까지 여름 9월부터 12월까지 가을 12월부터 3월전까지 겨울 
-
-=======
->>>>>>> fcabc7732272018719c282ac1deef268aca80f86
 let spring;
 let summer;
 let fall;
 let winter;
 
 let month = prompt("월을 입력하세요: ");
-<<<<<<< HEAD
+
 if(2 < month && month < 7) {
   console.log('spring');
 }
@@ -26,19 +23,3 @@ else if(8 < month && month < 12) {
 else {
   console.log('winter')
 }
-=======
-if(2 < month && month< 7) {
-    console.log('spring');
-}
-
-else if(6 < month && month < 10) {
-    console.log('summer')
-}
-
-else if(8 < month && month < 12) {
-    console.log('fall')
-}
-else {
-    console.log('winter')
-}
->>>>>>> fcabc7732272018719c282ac1deef268aca80f86
